3e0ac4c9d1d7eda8b290c8c2751407ff951fefc7
*f6Tl7nr3Os <phattt@hopee.com.vn>
2024-06-12 10:53:18 +0700

    HCM_PHP_06_05_2024-24 Create feature register and logout for client -fix route

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const router = createRouter({
       component: () => import('@/components/auth/FormRegister.vue')
     },
     {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue')
+    },
+    {
       path: '/:catchAll(.*)',
       name: 'notFound',
       component: () => import('@/components/NotFound.vue')
