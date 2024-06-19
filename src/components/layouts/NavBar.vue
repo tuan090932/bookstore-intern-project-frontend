48c33f47ad1c99c51db130f13a78ded46884aa8a
*f6Tl7nr3Os <phattt@hopee.com.vn>
2024-06-19 14:19:15 +0700

    HCM_PHP_06_05_2024-53 build feature update infor for client - fix show messeage

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -4,19 +4,27 @@
     class="header sticky top-0 bg-white shadow-md flex items-center justify-between px-8 py-2"
     style="z-index: 1"
   >
-    <!-- logo -->
-    <h1 class="w-3/12">
-      <router-link to="/">
-        <img
-          src="@/assets/logo.png"
-          alt="Logo"
-          class="h-20 w-auto hover:text-green-500 duration-200"
-        />
-      </router-link>
-    </h1>
+    <div class="flex items-center w-6/12 md:w-3/12">
+      <!-- mobile menu button -->
+      <div class="block md:hidden mr-2">
+        <button @click="toggleMobileMenu">
+          <font-awesome-icon :icon="['fas', 'bars']" />
+        </button>
+      </div>
+      <!-- logo -->
+      <h1>
+        <router-link to="/">
+          <img
+            src="@/assets/logo.png"
+            alt="Logo"
+            class="h-10 md:h-20 w-auto hover:text-green-500 duration-200"
+          />
+        </router-link>
+      </h1>
+    </div>
 
     <!-- navigation -->
-    <nav class="nav font-semibold text-lg">
+    <nav class="nav font-semibold text-lg hidden md:block">
       <ul class="flex items-center">
         <li
           class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer"
@@ -46,8 +54,29 @@
       </ul>
     </nav>
 
+    <!-- mobile menu -->
+    <nav v-if="isMobileMenuOpen" class="absolute top-16 left-0 w-full bg-white shadow-md md:hidden">
+      <ul class="flex flex-col items-center">
+        <li class="p-4 w-full text-center border-b">
+          <router-link to="/" @click="toggleMobileMenu">Home</router-link>
+        </li>
+        <li class="p-4 w-full text-center border-b">
+          <router-link to="/products" @click="toggleMobileMenu">Products</router-link>
+        </li>
+        <li class="p-4 w-full text-center border-b">
+          <router-link to="/collections" @click="toggleMobileMenu">Collections</router-link>
+        </li>
+        <li class="p-4 w-full text-center border-b">
+          <router-link to="/about" @click="toggleMobileMenu">About Us</router-link>
+        </li>
+        <li class="p-4 w-full text-center border-b">
+          <router-link to="/contact" @click="toggleMobileMenu">Contact Us</router-link>
+        </li>
+      </ul>
+    </nav>
+
     <!-- buttons --->
-    <div class="w-3/12 flex justify-end items-center">
+    <div class="w-6/12 md:w-3/12 flex justify-end items-center">
       <!--Button Search-->
       <router-link to="/search" class="mr-4">
         <font-awesome-icon :icon="['fas', 'search']" />
@@ -84,20 +113,26 @@ export default {
 
     const isLoggedIn = ref(!!userStore.token)
     const user = ref(userStore.user)
+    const isMobileMenuOpen = ref(false)
 
     onMounted(() => {
       userStore.loadUserFromLocalStorage()
     })
 
-    // Thêm watcher để theo dõi sự thay đổi của user
     watch(userStore, () => {
       isLoggedIn.value = !!userStore.token
       user.value = userStore.user
     })
 
+    const toggleMobileMenu = () => {
+      isMobileMenuOpen.value = !isMobileMenuOpen.value
+    }
+
     return {
       isLoggedIn,
-      user
+      user,
+      isMobileMenuOpen,
+      toggleMobileMenu
     }
   }
 }
