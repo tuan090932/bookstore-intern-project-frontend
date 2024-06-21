68a28cc5faff0a512066147ede3f6cbb9d7a7811
tuan090932 <gietconheo88@gmail.com>
2024-06-21 13:27:44 +0700

    HCM_PHP_06_05_2024-60 Create feature manage login sessions for users

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,20 +1,25 @@
 import axios from 'axios'
 import { useUserStore } from '@/stores/user'
-import router from '@/router/index' // Import router for navigation
+import router from '@/router/index'
+
 const api = axios.create({
   baseURL: 'http://localhost/api/'
 })
+
 api.interceptors.request.use(
   (config) => {
     const userStore = useUserStore()
     const token = userStore.token
-    config.headers['Authorization'] = `Bearer ${token}`
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
     return config
   },
   (error) => {
     return Promise.reject(error)
   }
 )
+
 api.interceptors.response.use(
   (response) => {
     return response
@@ -22,17 +27,29 @@ api.interceptors.response.use(
   (error) => {
     const userStore = useUserStore()
     const status = error.response?.status || 500
-    if (status === 401) {
-      console.log('Unauthorized access', status)
-      alert('You are not authorized to access this. Please log in again.')
-      userStore.clearUser()
-      router.push('/login')
-    } else if (status === 411) {
-      alert('Your login session has ended, please log in again.')
-      userStore.clearUser()
-      router.push('/login')
+
+    switch (status) {
+      case 401:
+        alert('You are not authorized to access this or session has ended. Please log in again.')
+        userStore.clearUser()
+        router.push('/login')
+        break
+      case 403:
+        alert('You are forbidden to access this resource.')
+        break
+      case 411:
+        alert('Your login session has ended, please log in again.')
+        userStore.clearUser()
+        router.push('/login')
+        break
+      default:
+        // Handle other errors or statuses as needed
+        break
     }
+
     return Promise.reject(error)
   }
 )
+
+
 export default api
