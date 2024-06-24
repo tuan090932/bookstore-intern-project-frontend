8f525a4ef65b87f03550f9e522be9fb8267506ec
*f6Tl7nr3Os <phattt@hopee.com.vn>
2024-06-24 16:52:43 +0700

    HCM_PHP_06_05_2024-74 Create feature add, delete favorite in FE

--- src/services/favorite.service.js
+++ src/services/favorite.service.js
@@ -1,18 +1,9 @@
 import api from './api.service';
-import { useUserStore } from '@/stores/user'; 
-import router from '@/router';
 
 const API_URL = 'favorite/';
 
 class FavoriteService {
   addFavorite(bookId) {
-    const userStore = useUserStore(); 
-    const token = userStore.token; 
-    if (!token) {
-      alert('Please login to add favorite');
-      router.push('/login');
-      return;
-    }
     return api.post(`${API_URL}`, { book_id: bookId });
   }
 
@@ -21,13 +12,6 @@ class FavoriteService {
   }
 
   deleteFavorite(favoriteId) {
-    const userStore = useUserStore(); 
-    const token = userStore.token;
-    if (!token) {
-        alert('Please login to delete favorite');
-        router.push('/login');
-        return;
-      }
     return api.delete(`${API_URL}${favoriteId}`);
   }
 }
