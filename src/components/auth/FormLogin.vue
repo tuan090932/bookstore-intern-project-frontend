1decd4fe7c2b4c0a6ecafd3a984a40c370872538
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-28 10:21:47 +0700

    HCM_PHP_06_05_2024_70_Modify_to_be_able_to_login_with_username_frontend

--- src/components/auth/FormLogin.vue
+++ src/components/auth/FormLogin.vue
@@ -2,36 +2,36 @@
   <div class="min-h-screen flex items-center justify-center w-full bg-gray-100">
     <div class="bg-white shadow-md rounded-lg px-8 py-6 max-w-md">
       <h1 class="text-2xl font-bold text-center mb-4 text-gray-900">Welcome Back!</h1>
-      <form @submit.prevent="handleSubmit">
+      <Form @submit="handleSubmit" v-slot="{ errors }">
         <!-- Email or Username -->
-        <div class="mb-4">
+        <div class="mb-4 w-80">
           <label for="emailOrUsername" class="block text-sm font-medium text-gray-700 mb-2"
             >Email Address or Username</label
           >
-          <input
-            v-model="emailOrUsername"
+          <Field
+            name="emailOrUsername"
             type="text"
             id="emailOrUsername"
             class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="your@email.com or username"
-            required
+            rules="required"
+            v-model="emailOrUsername"
           />
-          <div v-if="errors.emailOrUsername" class="mt-1 text-red-500 text-sm">
-            {{ errors.emailOrUsername }}
-          </div>
+          <ErrorMessage name="emailOrUsername" class="mt-1 text-red-500 text-sm" />
         </div>
 
         <!-- Password -->
-        <div class="mb-4 relative">
+        <div class="mb-4 relative w-80">
           <label for="password" class="block text-sm font-medium text-gray-700 mb-2">Password</label>
           <div class="relative w-80">
-            <input
-              v-model="password"
+            <Field
+              name="password"
               :type="showPassword ? 'text' : 'password'"
               id="password"
-              class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              class="shadow-sm rounded-md w-80 px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 "
               placeholder="Enter your password"
-              required
+              :rules="{ required: true, password: true }"
+              v-model="password"
             />
             <button
               type="button"
@@ -41,24 +41,22 @@
               <font-awesome-icon :icon="showPassword ? 'eye-slash' : 'eye'" />
             </button>
           </div>
-          <div v-if="errors.password" class="mt-1 text-red-500 text-sm w-80">
-            {{ errors.password }}
-          </div>
-          <a
-            href="#"
-            class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >Forgot Password?
-          </a>
+          <ErrorMessage name="password" class="mt-1 text-red-500 text-sm" />
         </div>
+        <a
+          href="#"
+          class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >Forgot Password?
+        </a>
 
         <!-- Remember me -->
-        <div class="flex items-center justify-between mb-4">
+        <div class="flex items-center justify-between mb-4 mt-3">
           <div class="flex items-center">
-            <input
+            <Field
               type="checkbox"
+              name="remember"
               id="remember"
               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
-              checked
             />
             <label for="remember" class="ml-2 block text-sm text-gray-700">Remember me</label>
           </div>
@@ -73,90 +71,74 @@
         <button
           type="submit"
           class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          :disabled="Object.keys(errors).length > 0"
         >
           Login
         </button>
-      </form>
+      </Form>
     </div>
   </div>
 </template>
 
 <script>
-import AuthService from '@/services/auth.service'
-import { useUserStore } from '@/stores/user'
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { defineRule } from 'vee-validate';
+import AuthService from '@/services/auth.service';
+import { useUserStore } from '@/stores/user';
 
 export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data() {
     return {
       emailOrUsername: '',
       password: '',
       showPassword: false,
-      errors: {
-        emailOrUsername: '',
-        password: ''
-      }
-    }
+    };
   },
   computed: {
     userStore() {
-      return useUserStore()
-    }
+      return useUserStore();
+    },
+  },
+  mounted() {
+    defineRule('password', (value) => {
+      const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}:'"\\|,.<>/?])(?=.*[a-z]).{6,}$/;
+      if (!regex.test(value)) {
+        return 'Password must include 1 uppercase, 1 number, 1 special character, and be 6+ characters long';
+      }
+      return true;
+    });
   },
   methods: {
     togglePasswordVisibility() {
-      this.showPassword = !this.showPassword
+      this.showPassword = !this.showPassword;
     },
-    validatePassword(password) {
-      const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}:'"\\|,.<>/?])(?=.*[a-z]).{6,}$/;
-      return regex.test(password);
-    },
-    async handleSubmit() {
-      this.clearErrors()
-      
-      if (!this.emailOrUsername) {
-        this.errors.emailOrUsername = 'Please enter email or username'
-      }
-      
-      if (!this.password) {
-        this.errors.password = 'Please enter password'
-      } else if (!this.validatePassword(this.password)) {
-        this.errors.password = 'Password must include 1 uppercase, 1 number, 1 special character, and be 6+ characters long'
-      }
-
-      if (this.hasErrors()) {
-        return
-      }
-
+    async handleSubmit(values) {
       try {
-        const response = await AuthService.login(this.emailOrUsername, this.password)
-        const token = response.access_token
-        this.userStore.setToken(token)
-        console.log('Login success!')
-        await this.getUserProfile(token)
-        this.$router.push('/profile')
+        const response = await AuthService.login(values.emailOrUsername, values.password);
+        const token = response.access_token;
+        this.userStore.setToken(token);
+        console.log('Login success!');
+        await this.getUserProfile(token);
+        this.$router.push('/profile');
       } catch (error) {
-        this.errors.emailOrUsername = 'Email or username and password is incorrect'
+        console.error('Login failed:', error);
       }
-    },
-    clearErrors() {
-      this.errors = {
-        emailOrUsername: '',
-        password: ''
-      }
-    },
-    hasErrors() {
-      return Object.values(this.errors).some(error => error !== '')
     },
     async getUserProfile(token) {
       try {
-        const user = await AuthService.getProfile(token)
-        this.userStore.setUser(user)
+        const user = await AuthService.getProfile(token);
+        this.userStore.setUser(user);
       } catch (error) {
-        console.error('Failed to fetch user profile:', error)
+        console.error('Failed to fetch user profile:', error);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
